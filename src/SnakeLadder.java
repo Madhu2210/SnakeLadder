889ca05a8a0eb01f02fad828aa65f6f917bd5955
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-21 20:29:06 +0530

    Added: public static final int START_POSITION = 0;

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -6,31 +6,35 @@ public class SnakeLadder {
 
     static int playerPosition = 0;
     static int diceNumber = 0;
+    static int totalDiceRoll=0;
 
     public static void setPlayerMoves() {
         while (playerPosition < 100) {
             diceNumber = (int) ((Math.random() * 10 % 6) + 1);
             int option = (int) ((Math.random() * 10 % 3) + 1);
 
+
             if (option == IS_SNAKE) {
                 playerPosition -= diceNumber;
-            } else if (option == IS_LADDER) {
+            }
+            else if (option == IS_LADDER) {
                 playerPosition += diceNumber;
-            } else if (option == NO_PLAY) {
+            }
+            else if (option == NO_PLAY) {
                 System.out.println("NO Play");
             }
 
             if (playerPosition < START_POSITION) {
                 playerPosition = START_POSITION;
-            } else if (playerPosition > 100) {
+            }else if(playerPosition>100){
                 playerPosition -= diceNumber;
             }
+            totalDiceRoll++;
             System.out.println("Player position: " + playerPosition);
         }
     }
-
-    public static void playUntilWin() {
-        while (playerPosition != 100) {
+    public static void  playUntilWin(){
+        while (playerPosition != 100){
             setPlayerMoves();
         }
     }
@@ -38,6 +42,7 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
         playUntilWin();
-        System.out.println("Player has won");
+        System.out.println("Player has won" ) ;
+        System.out.println("Total dice roll: "+totalDiceRoll);
     }
 }
