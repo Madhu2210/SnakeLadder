9cc3dac406b27eb555e08a1827ecbe1d77ed548f
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-21 18:19:05 +0530

    Added: Player rolls the die to get the value

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -2,5 +2,7 @@ public class SnakeLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
         int startPosition =0;
+        int diceNumber = (int) ((Math.random() * 10 % 6) +1);
+        System.out.println("dice Number:"+ diceNumber);
     }
 }
