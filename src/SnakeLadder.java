0c3d3a439bd70ecfd283493f28e9449c0f40ede1
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-21 18:39:49 +0530

    Added: Created function to check No play or ladder or snake

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,8 +1,20 @@
 public class SnakeLadder {
+    public static final int START_POSITION =0;
+    public static final int IS_SNAKE =1;
+    public static final int IS_LADDER =2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder Game");
-        int startPosition =0;
+        int currentPosition =0;
         int diceNumber = (int) ((Math.random() * 10 % 6) +1);
-        System.out.println("dice Number:"+ diceNumber);
+        int option = (int)  ((Math.random() * 10 % 3) +1);
+
+        if (option == IS_SNAKE){
+            currentPosition -= diceNumber;
+        }else if(option == IS_LADDER) {
+            currentPosition += diceNumber;
+        }
+        else {
+            System.out.println("NO Play");
+        }
     }
 }
